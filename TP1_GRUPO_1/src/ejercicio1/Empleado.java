25adf7e8cca89964d991ea9e81b202dad8b29059
Lunaaoaba <lunabaricalla1@gmail.com>
2026-08-18 14:47:53 -0300

    Fix: Nuevamente la ubicación
    Se subió el tp duplicado dentro de otra subcarpeta

--- TP1_GRUPO_1/src/ejercicio1/Empleado.java
+++ TP1_GRUPO_1/src/ejercicio1/Empleado.java
@@ -1,10 +1,36 @@
 package ejercicio1;
+import java.time.LocalDate;
 
 public class Empleado extends Persona {
-    private int legajo;
+    private static int contadorLegajo = 1000;
+    private final int legajo;
     private String puesto;
 
     public Empleado() {
         super();
+        this.legajo = contadorLegajo++;
+        this.puesto = " Sin puesto ";
     }
-}
+    
+    public Empleado(String puesto, String Dni, String Nombre, String Apellido, int Edad, LocalDate FechaNacimiento,
+			        String Genero, String Direccion, String Telefono, String Email) {
+    	super(Dni, Nombre, Apellido, Edad, FechaNacimiento, Genero, Direccion, Telefono, Email);
+    	this.legajo = contadorLegajo++; 
+    	this.puesto = puesto;
+    }
+
+	public String getPuesto() {
+		return puesto;
+	}
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+	public int getLegajo() {
+		return legajo;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [legajo=" + legajo + ", puesto=" + puesto + "]" + super.toString();
+	} 
+} 
