239f609354b4cc4d1d89952e8d18e2ec8ee45a64
eastanganelli <ezequielstanganelli@gmail.com>
2026-08-19 00:37:24 -0300

    Agregado de Empleado por defecto e impresion

--- TP1_GRUPO_1/src/ejercicio1/Ejercicio_1.java
+++ TP1_GRUPO_1/src/ejercicio1/Ejercicio_1.java
@@ -5,10 +5,17 @@ public class Ejercicio_1 {
         Empleado empleado1 = new Empleado("Gerente", "12345678", "Juan", "Perez", 30, null, "Masculino", "Calle 123", "123456789", "juan.perez@email.com");
         System.out.println(empleado1.toString());
         
+        // Impresión del proximo legajo
+        System.out.println("El proximo legajo es: " + Empleado.devuelveProximoLegajo());
+
         Empleado empleado2 = new Empleado("Supervisor", "91011121", "Alberto", "Gutierrez", 40, null, "Masculino", "Calle 456", "1145342124", "albertito.guti@gmail.com");
         System.out.println(empleado2.toString());
         
-     
-        
+        // Impresión del proximo legajo
+        System.out.println("El proximo legajo es: " + Empleado.devuelveProximoLegajo());
+
+        // Impresion de empeado sin parametros o por defecto
+        Empleado empleado3 = new Empleado();
+        System.out.println(empleado3.toString());
     }
 }
