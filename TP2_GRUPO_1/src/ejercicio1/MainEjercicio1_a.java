585679fd2edc5d04b220d995ae4bdea89b155bd7
LeoSBenitez <54222448+LeoSBenitez@users.noreply.github.com>
2026-09-07 17:16:01 -0300

    Leonardo (#35)
    * Agregar proyecto del TP1
    
    Se agrega el proyecto Java de consola con el nombre solicitado para el TP1 (TP1_GRUPO_X) dejando la estructura inicial para que los integrantes puedan comenzar a trabajar (Crear paquete, clases)
    
    * Cambio Nombre Proyecto
    
    Habia un error en el nombre del proyecto, modifique la X por el numero de nuestro grupo.
    
    * Create Persona.java
    
    * Commit de Sarah
    
    Agregue los cambios de Sarah que describio en el foro
    
    * Cambios de Sarah agregados Github
    
    Cambios de Sarah agregados Github ahora si creo sin errores
    
    * Finalización subpunto 2 y subpunto 3
    
    Modifique las variable legajo, cree una nueva para el subpunto 3, cree los setters y getters necesarios de Empleado y el metodo toString que hereda de Persona a la vez.
    
    Añadi los cambios de Fran que no se porque no estaban en la clase Persona
    
    * 4b
    
    Cree segunda persona salio todo bien y modifique mensajes con lo que pedia el enunciado
    
    * Correciones
    
    Correciones ejercicio 1 mainejercicio1a b y modificaciones en persona.java para exVerificarDni

--- TP2_GRUPO_1/src/ejercicio1/MainEjercicio1_a.java
+++ TP2_GRUPO_1/src/ejercicio1/MainEjercicio1_a.java
@@ -7,7 +7,8 @@ public class MainEjercicio1_a {
         
         Persona persona1 = new Persona("AA202020", "Juan", "Pérez", 30, null, "Masculino", "Calle 123", "123456789", "juan.perez@gmail.com");
         try {
-            Persona.exVerificarDNI(persona1);
+        	Persona.exVerificarDNI(persona1.getDni());
+        	System.out.println("La Persona \"" + persona1.getNombre() + " " + persona1.getApellido() + "\" con DNI \"" + persona1.getDni() + "\" fue agregada correctamente");
         } catch (ExVerificarDNI e) {
             System.out.println("Error al crear la persona " + persona1.getNombre() + " " + persona1.getApellido() + ": " + e.getMessage());
         }
@@ -15,7 +16,8 @@ public class MainEjercicio1_a {
         // 4b crear la 2da persona y deberia dar dni correcto
         Persona persona2 = new Persona("20202020", "Germán", "Martitegui", 60, null, "Masculino", "Delta del Tigre", "1134567214", "bestChefARG@gmail.com");
         try {
-            Persona.exVerificarDNI(persona2);
+        	Persona.exVerificarDNI(persona2.getDni());
+        	System.out.println("La Persona \"" + persona2.getNombre() + " " + persona2.getApellido() + "\" con DNI \"" + persona2.getDni() + "\" fue agregada correctamente");
         } catch (ExVerificarDNI e) {
             System.out.println("Error al crear la persona " + persona2.getNombre() + " " + persona2.getApellido() + ": " + e.getMessage());
         }
